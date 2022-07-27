2ee79ceb23c2ae8ac983a2eebe6ca684f45ed4f6
Ly Quoc Dung <quocdunglxag123@gmail.com>
2022-07-27 09:20:12 +0700

    upload project

---
+++ src/main/java/fa/trainning/TheGioiDiDongNhom7Application.java
@@ -0,0 +1,13 @@
+package fa.trainning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGioiDiDongNhom7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGioiDiDongNhom7Application.class, args);
+	}
+
+}
---
+++ src/test/java/fa/trainning/TheGioiDiDongNhom7ApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.trainning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGioiDiDongNhom7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
