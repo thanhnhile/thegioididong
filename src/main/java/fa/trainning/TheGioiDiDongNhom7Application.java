82884a703e16707c1c1b958d5604e8b366080d74
thanhnhile <marrythanhnhipl205@gmail.com>
2022-07-27 10:44:57 +0700

    test

--- src/main/java/fa/trainning/TheGioiDiDongNhom7Application.java
+++ src/main/java/fa/trainning/TheGioiDiDongNhom7Application.java
@@ -2,8 +2,10 @@ package fa.trainning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class TheGioiDiDongNhom7Application {
 
 	public static void main(String[] args) {
