90d1072f783d8024ea786461517d8d6b23e59713
thanhnhile <marrythanhnhipl205@gmail.com>
2022-08-05 08:47:32 +0700

    / oder

--- src/main/java/fa/trainning/controller/OrderDetailController.java
+++ src/main/java/fa/trainning/controller/OrderDetailController.java
@@ -1,31 +1,31 @@
-package fa.trainning.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import fa.trainning.dto.OrderDetailDto;
-import fa.trainning.service.impl.OrderDetailServiceImpl;
-
-@RestController
-@RequestMapping(value="/details")
-public class OrderDetailController {
-	
-	@Autowired
-	private OrderDetailServiceImpl detailServiceImpl;
-	
-	@GetMapping(value="/{id}")
-	public OrderDetailDto getOrderDetail(@PathVariable Integer id) {
-		return detailServiceImpl.getOrderDetail(id);
-	}
-	
-	@PostMapping
-	public OrderDetailDto addOrderDetail(@Validated @RequestBody OrderDetailDto detailDto) {
-		return detailServiceImpl.addOrderDetail(detailDto);
-	}
-}
+//package fa.trainning.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import fa.trainning.dto.OrderDetailDto;
+//import fa.trainning.service.impl.OrderDetailServiceImpl;
+//
+//@RestController
+//@RequestMapping(value="/details")
+//public class OrderDetailController {
+//	
+//	@Autowired
+//	private OrderDetailServiceImpl detailServiceImpl;
+//	
+//	@GetMapping(value="/{id}")
+//	public OrderDetailDto getOrderDetail(@PathVariable Integer id) {
+//		return detailServiceImpl.getOrderDetail(id);
+//	}
+//	
+//	@PostMapping
+//	public OrderDetailDto addOrderDetail(@Validated @RequestBody OrderDetailDto detailDto) {
+//		return detailServiceImpl.addOrderDetail(detailDto);
+//	}
+//}
--- src/main/java/fa/trainning/service/OrderDetailService.java
+++ src/main/java/fa/trainning/service/OrderDetailService.java
@@ -1,13 +1,13 @@
-package fa.trainning.service;
-
-import java.util.List;
-
-import fa.trainning.dto.OrderDetailDto;
-
-public interface OrderDetailService {
-	List<OrderDetailDto> getOrderDetailsByOrderId(Integer orderId);
-	
-	OrderDetailDto getOrderDetail(Integer id);
-	
-	OrderDetailDto addOrderDetail(OrderDetailDto orderDetailDto);
-}
+//package fa.trainning.service;
+//
+//import java.util.List;
+//
+//import fa.trainning.dto.OrderDetailDto;
+//
+//public interface OrderDetailService {
+//	List<OrderDetailDto> getOrderDetailsByOrderId(Integer orderId);
+//	
+//	OrderDetailDto getOrderDetail(Integer id);
+//	
+//	OrderDetailDto addOrderDetail(OrderDetailDto orderDetailDto);
+//}
