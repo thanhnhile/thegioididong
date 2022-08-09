5391c9a71882e68c8219d38341ad8ce052d1478b
loc24 <110075352+loc24@users.noreply.github.com>
2022-08-09 08:46:21 +0700

    CRUD Customer

--- src/main/java/fa/trainning/dto/CustomerDto.java
+++ src/main/java/fa/trainning/dto/CustomerDto.java
@@ -1,5 +1,59 @@
 package fa.trainning.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CustomerDto {
+	@JsonProperty("customerID")
+	private Integer id;
+
+	@JsonProperty("customerName")
+	private String name;
+
+	@JsonProperty("customerAddress")
+	private String address;
+
+	@JsonProperty("customerPhone")
+	private String phone;
+
+	public CustomerDto(Integer id, String name, String address, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCustomerName() {
+		return name;
+	}
+
+	public void setCustomerName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
+
