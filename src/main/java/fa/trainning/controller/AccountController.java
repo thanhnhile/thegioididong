6391607be24035d8732f0c4e9ed982ded7a0a19b
Ly Quoc Dung <quocdunglxag123@gmail.com>
2022-08-11 05:59:01 +0700

    upload exception

--- src/main/java/fa/trainning/controller/AccountController.java
+++ src/main/java/fa/trainning/controller/AccountController.java
@@ -1,8 +1,15 @@
 package fa.trainning.controller;
 
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +20,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fa.trainning.dto.AccountDto;
+import fa.trainning.dto.AccountTokenResponse;
 import fa.trainning.dto.DataResponse;
+import fa.trainning.entity.Account;
+import fa.trainning.security.JwtTokenUtil;
 import fa.trainning.service.impl.AccountServiceImpl;
 
 @RestController
@@ -22,19 +32,40 @@ public class AccountController {
 	@Autowired
 	private AccountServiceImpl accountImpl;
 
+	@Autowired
+	AuthenticationManager authManager;
+	@Autowired
+	JwtTokenUtil jwtUtil;
+
+	@PostMapping("/login")
+	public DataResponse login(@RequestBody @Valid AccountDto accountDto) {
+			Authentication authentication = authManager.authenticate(
+					new UsernamePasswordAuthenticationToken(
+							accountDto.getUsername(), accountDto.getPassword())
+			);
+			
+			Account account = (Account) authentication.getPrincipal();
+			String accessToken = jwtUtil.generateAccessToken(account);
+			AccountTokenResponse response = new AccountTokenResponse(account.getUsername(), accessToken);
+			
+			return new DataResponse(response);
+			
+		
+	}
+
 	@GetMapping()
 	public DataResponse getAccount() {
-		return  new DataResponse( accountImpl.getAllAccount());
+		return new DataResponse(accountImpl.getAllAccount());
 	}
 
 	@GetMapping("/{id}")
 	public DataResponse getAccount(@PathVariable(value = "id") Integer id) {
-		return  new DataResponse( accountImpl.getAccount(id));
+		return new DataResponse(accountImpl.getAccount(id));
 	}
 
-	@PostMapping("/access")
+	@PostMapping()
 	public DataResponse addAccount(@RequestBody AccountDto accountDto) {
-		return  new DataResponse( accountImpl.addAccount(accountDto));
+		return new DataResponse(accountImpl.addAccount(accountDto));
 	}
 
 	@DeleteMapping("/{id}")
@@ -44,7 +75,7 @@ public class AccountController {
 
 	@PutMapping("/{id}")
 	public DataResponse updateAccount(@PathVariable(value = "id") Integer id, @RequestBody AccountDto accountDto) {
-		return  new DataResponse(accountImpl.updateAccount(id, accountDto));
+		return new DataResponse(accountImpl.updateAccount(id, accountDto));
 	}
 
 }
